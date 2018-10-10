fd4e388fa9554993d482198eac91944b2fe22ce2
Dragos Sandu <dragos.n.sandu@gmail.com>
2018-10-10 18:59:21 +0300

    Create /command GET response

--- server.js
+++ server.js
@@ -10,18 +10,21 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    if ( command > 0 )
-        res.send("Muie Dragnea");
-    else
         res.render('index');
+        //res.sendStatus(command);
+        // res.send(command);
 });
 
 app.post('/',(req,res)=>{
 
 command = req.body.powerOn; // here you can get the value of from the textbox
+    res.render('index');
     //res.send(command);
-    res.json({command: command})
+    //res.json({command: command}
+});
 
+app.get('/command',(req,res)=>{
+    res.send(command.toString());
 });
 
 app.listen(port, () => console.log('Server is running on port ' + port));
